39d2831dbea066ee98c27d9aaf06d92b3a333ac6
Bioflora <62134347+Bioflora@users.noreply.github.com>
2021-07-14 15:25:48 +0200

    Add files via upload

---
+++ Brachypodium_Bdhn_CREs/cluster_finales.tf
@@ -0,0 +1,140 @@
+AC  cluster_1_node_10_11motifs
+XX
+ID  cluster_1_node_10_11motifs
+XX
+DE  symCACGTACsss----------
+P0           a         c         g         t
+1           53       242       194        21
+2           94       173       135       202
+3          287       217       100         0
+4            0       596         8         0
+5          599         0         1         4
+6           10       582         0        12
+7            1         1       602         0
+8            1        11         4       588
+9          578        14        12         0
+10          16       563        20         5
+11         143       204       175        82
+12          82       257       173        92
+13          10        86        47        23
+14           0         0         0         0
+15           0         0         0         0
+16           0         0         0         0
+17           0         0         0         0
+18           0         0         0         0
+19           0         0         0         0
+20           0         0         0         0
+21           0         0         0         0
+22           0         0         0         0
+23           0         0         0         0
+XX
+CC  program: transfac
+CC  matrix.nb: 1
+CC  accession: cluster_1_node_10_11motifs
+CC  AC: cluster_1_node_10_11motifs
+CC  id: cluster_1_node_10_11motifs
+CC  name: cluster_1_node_10_11motifs
+CC  version: 
+CC  name: cluster_1_node_10_11motifs
+CC  description: symCACGTACsss----------
+CC  transfac_consensus: 
+CC  matrix.nb: 1
+CC  consensus.strict: ctaCACGTACccc----------
+CC  consensus.strict.rc: ----------GGGGTACGTGTAG
+CC  consensus.IUPAC: symCACGTACsss----------
+CC  consensus.IUPAC.rc: ----------SSSGTACGTGKRS
+CC  consensus.regexp: [cg][ct][ac]CACGTAC[cg][cg][cg]----------
+CC  consensus.regexp.rc: ----------[CG][CG][CG]GTACGTG[GT][AG][CG]
+CC  type: merge
+CC  merge_type: sum
+CC  merge_nb: 11
+CC  merged_AC: 'bra_motif_names.sig:::oligos_6nt_test_vs_ctrl_m2,bra_motif_names.sig:::oligos_6nt_test_vs_ctrl_m2,bra_motif_names.sig:::oligos_6nt_test_vs_ctrl_m3,bra_motif_names.sig:::dyads_test_vs_ctrl_m3,bra_motif_names.sig:::dyads_test_vs_ctrl_m2,bra_motif_names.sig:::oligos_6nt_test_vs_ctrl_m2,bra_motif_names.sig:::dyads_test_vs_ctrl_m2,bra_motif_names.sig:::dyads_test_vs_ctrl_m2,bra_motif_names.sig:::oligos_7nt_test_vs_ctrl_m1,bra_motif_names.sig:::oligos_7nt_test_vs_ctrl_m1,bra_motif_names.sig:::oligos_7nt_test_vs_ctrl_m1'
+CC  merged_ID: 'M2_Bhyb_HBI1,M2_Bdis_HBI1,M3_Bsta_HBI1,M13_Bsta_ANAC55,M12_Bdis_ANAC55,M2_Bsyl_HYH,M12_Bsyl_HYH,M12_Bhyb_BES1,M6_Bsta_BES1,M6_Bhyb_BES1,M6_Bsyl_BES1'
+XX
+//
+AC  cluster_2
+XX
+ID  cluster_2
+XX
+DE  ksGACGCGTCsm
+P0           a         c         g         t
+1           27        51        87        99
+2           60        96       105         3
+3            0        15       246         3
+4          201         6        15        42
+5            0       243         0        21
+6            3         0       261         0
+7            0       261         0         3
+8           21         0       243         0
+9           42         9         6       207
+10           3       240        21         0
+11           3       105        96        60
+12         111        75        51        27
+XX
+CC  program: transfac
+CC  matrix.nb: 1
+CC  accession: cluster_2
+CC  AC: cluster_2
+CC  id: cluster_2
+CC  name: cluster_2
+CC  version: 
+CC  name: cluster_2
+CC  description: ksGACGCGTCsm
+CC  transfac_consensus: 
+CC  matrix.nb: 1
+CC  consensus.strict: tgGACGCGTCca
+CC  consensus.strict.rc: TGGACGCGTCCA
+CC  consensus.IUPAC: ksGACGCGTCsm
+CC  consensus.IUPAC.rc: KSGACGCGTCSM
+CC  consensus.regexp: [gt][cg]GACGCGTC[cg][ac]
+CC  consensus.regexp.rc: [GT][CG]GACGCGTC[CG][AC]
+CC  type: merge
+CC  merge_type: sum
+CC  merge_nb: 3
+CC  merged_AC: 'bra_motif_names.sig:::oligos_7nt_test_vs_ctrl_m5,bra_motif_names.sig:::oligos_6nt_test_vs_ctrl_m4,bra_motif_names.sig:::oligos_6nt_test_vs_ctrl_m4'
+CC  merged_ID: 'M10_Bsta_MYB124,M4_Bhyb_MYB124,M4_Bdis_MYB124'
+XX
+//
+AC  cluster_3
+XX
+ID  cluster_3
+XX
+DE  AbTAGTGTAdwt
+P0           a         c         g         t
+1           34         1         5         8
+2            4        12        20        12
+3            0         1         7        40
+4           48         0         0         0
+5            0         0        48         0
+6            0         0         0        48
+7            0         0        48         0
+8            0         0         0        48
+9           48         0         0         0
+10          12         0        12        24
+11          18        10         4        16
+12           3         1         3         7
+XX
+CC  program: transfac
+CC  matrix.nb: 1
+CC  accession: cluster_3
+CC  AC: cluster_3
+CC  id: cluster_3
+CC  name: cluster_3
+CC  version: 
+CC  name: cluster_3
+CC  description: AbTAGTGTAdwt
+CC  transfac_consensus: 
+CC  matrix.nb: 1
+CC  consensus.strict: AgTAGTGTAtat
+CC  consensus.strict.rc: ATATACACTACT
+CC  consensus.IUPAC: AbTAGTGTAdwt
+CC  consensus.IUPAC.rc: AWHTACACTAVT
+CC  consensus.regexp: A[cgt]TAGTGTA[agt][at]t
+CC  consensus.regexp.rc: A[AT][ACT]TACACTA[ACG]T
+CC  type: merge
+CC  merge_type: sum
+CC  merge_nb: 4
+CC  merged_AC: 'bra_motif_names.sig:::oligos_6nt_test_vs_ctrl_m1,bra_motif_names.sig:::dyads_test_vs_ctrl_m1,bra_motif_names.sig:::dyads_test_vs_ctrl_m1,bra_motif_names.sig:::dyads_test_vs_ctrl_m1'
+CC  merged_ID: 'M1_Bdis_ZAT7,M11_Bsta_ZAT6,M11_Bhyb_ZAT6,M11_Bsyl_ZAT6'
+XX
+//
